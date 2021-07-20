ef7f615871f8cd541954e87c39f5fcb6bfae7c95
Brian Barr <brian.barr23@gmail.com>
2021-07-20 18:41:30 -0400

    completed volumes and docker-compose

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hey does this work?</p>
+        <h3>Docker Daddy What WHAAAAT?</h3>
         <a
           className='App-link'
           href='https://reactjs.org'
