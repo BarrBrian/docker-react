86e585d2d00e930c01424766200bad9e86f7cb11
Brian Barr <brian.barr23@gmail.com>
2021-07-22 19:32:10 -0400

    small copy change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hey does this work?</p>
+        <p>Hi dad - I make internet do</p>
         <a
           className='App-link'
           href='https://reactjs.org'
